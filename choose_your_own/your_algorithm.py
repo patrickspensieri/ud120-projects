91ce141792245e1f68abbea7d1964e20301b6aec
patrickspensieri <patrick.spensieri@gmail.com>
2017-04-21 14:53:20 -0400

    complete decision trees and choose your own algorithm lessons

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 
+from time import time
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -17,28 +21,45 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
 ################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
-### visualization code (prettyPicture) to show you the decision boundary
+#start knn
+knn = KNeighborsClassifier(weights="distance", n_neighbors=10)
+t0 = time()
+knn.fit(features_train, labels_train)
+print "training knn : ", round(time() - t0), "s"
+t0 = time()
+predKnn = knn.predict(features_test)
+print "prediction knn : ", round(time() - t0), "s"
+scoreKnn = accuracy_score(labels_test, predKnn)
+print "score knn : ", scoreKnn
+#end knn
+
+#start random forest
+rf = RandomForestClassifier(n_estimators=10, min_samples_split=20)
+t0 = time()
+rf.fit(features_train, labels_train)
+print "training rf : ", round(time() - t0), "s"
+t0 = time()
+predRf = rf.predict(features_test)
+print "prediction rf : ", round(time() - t0), "s"
+scoreRf = accuracy_score(labels_test, predRf)
+print "score rf : ", scoreRf
+#end random forest
+
+clf = knn
 
 
-
-
-
-
-
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+# try:
+#     prettyPicture(clf, features_test, labels_test)
+# except NameError:
+#     pass
--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,20 +12,32 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+#reduce size of training data to 1/100 of original, lower precision
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
+#num columns = num features
+#num features can be tuned in email_preprocess.py,
+#by lowering percentile param in SelectPercentile
+print "number of features : ", len(features_test[0])
 
+#code starts here
+clf = DecisionTreeClassifier(min_samples_split=40)
 
+#train the classifier
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time : ", round(time() - t0, 3), "s"
 
-#########################################################
-### your code goes here ###
+#predict new labels
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time : ", round(time() - t0), "s"
+print accuracy_score(labels_test, pred)
 
-
-#########################################################
 
 
